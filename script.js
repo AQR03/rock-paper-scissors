78fb9e30264dd02004cae15f95ec28a6c8370067
Andon <aqr03@outlook.com>
2026-06-02 23:09:27 -0400

    added if statement to the getComputerChoice function to turn the random number into a choice of rock paper or scissors

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    console.log(choice);
+    
+    if (choice === 0) {
+        console.log('Rock');
+    } else if (choice === 1) {
+        console.log('Paper');
+    } else {
+        console.log('Scissors');
+    }
 }
 
+getComputerChoice();
