14a8eef06a99a01e6bb85f74df131ff7bf95d55d
Andon <aqr03@outlook.com>
2026-06-03 00:06:42 -0400

    introduced a scoring system that tracks playerScore and computerScore

--- script.js
+++ script.js
@@ -16,9 +16,25 @@ function getPlayerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log('It\'s a tie');
+    } else if (
+        humanChoice === 'rock' && computerChoice === 'scissors' ||
+        humanChoice === 'paper' && computerChoice === 'rock' ||
+        humanChoice === 'scissors' && computerChoice === 'paper'
+    ) {
+        playerScore++
+        console.log('Congrats you won!');
+    } else {
+        computerScore++
+        console.log('Sorry you lost. Try again.');
+    }
+    console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`)
     console.log(`Player: ${humanChoice}\nComputer: ${computerChoice}`)
 }
 
+let playerScore = 0
+let computerScore = 0
 const humanChoice = getPlayerChoice();
 const computerChoice = getComputerChoice();
 
